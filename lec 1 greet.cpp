5a644ecb810f5726d944896b31251af561ce8be5
OmarAttia-code <omarattia007@gmail.com>
2026-08-03 14:09:14 +0300

    lecture 1 greet code

---
+++ lec 1 greet.cpp
@@ -0,0 +1,13 @@
+// lec 1 get name & greet 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string answer;
+    cout << "What's your name? ";
+    cin >> answer;
+    cout << "hello, " << answer << "\n";
+    return 0;
+}
